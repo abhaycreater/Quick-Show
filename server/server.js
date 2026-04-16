1abc51f2e869ec130dc2245b00a8b60826ed16ec
abhaycreater <patereabhay@gmail.com>
2026-04-16 21:01:17 +0530

    fix inngest route

--- server/server.js
+++ server/server.js
@@ -1,26 +1,27 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './configs/db.js'
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./Ingest/index.js"
+import { inngest, functions } from "./Ingest/index.js";
 
-const app = express()
+const app = express();
 const port = 3000;
 
-await connectDB()
+await connectDB();
 
-//middleware
-app.use(express.json())
-app.use(cors())
-app.use(clerkMiddleware())
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 
-//api routs
-app.get('/', (req , res)=> res.send('server is Live'))
+// routes
+app.get("/", (req, res) => res.send("server is Live"));
 
-app.use('/api/ingest',  serve({ client: inngest, functions }))
+// ✅ FIXED ROUTE
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.listen(port,()=>{
-    console.log(`server Live On Port http://localhost:${port}`);   
-} )
+app.listen(port, () => {
+  console.log(`server Live On Port http://localhost:${port}`);
+});
